12b4ab82c33d209f73a05c76fb72b8dc173970c7
ToledoZz <cerdasfallasj@gmail.com>
2026-04-24 11:28:53 -0600

    Programa - Primer Commit

--- src/main/java/com/example/demo/controller/ComercioController.java
+++ src/main/java/com/example/demo/controller/ComercioController.java
@@ -17,18 +17,24 @@ public class ComercioController {
     private ComercioService service;
 
     @GetMapping
-    public List<Comercio> listar(){
+    public List<Comercio> listar() {
         return service.listar();
     }
 
     @PostMapping
-    public Comercio guardar(@RequestBody Comercio comercio){
+    public Comercio guardar(@RequestBody Comercio comercio) {
+        comercio.setIdComercio(null); // asegurar creación nueva
+        return service.guardar(comercio);
+    }
+
+    @PutMapping("/{id}")
+    public Comercio editar(@PathVariable Integer id, @RequestBody Comercio comercio) {
+        comercio.setIdComercio(id);
         return service.guardar(comercio);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Integer id){
+    public void eliminar(@PathVariable Integer id) {
         service.eliminar(id);
     }
-
 }
